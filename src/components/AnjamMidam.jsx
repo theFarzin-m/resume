2c7d74a265a624db83c11cd791f611c2157ec905
farzin abbasi <frzin.af@gmail.com>
2024-12-02 16:50:07 +0330

    temporary changes

---
+++ src/components/AnjamMidam.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AnjamMidam() {
+  return (
+    <div className='d-flex flex-column justify-content-center align-items-center vh-100'>
+    <img className='img-fluid' src='https://anjammidam.com/media/cache/thumb15/uploads/user/images/147d373a8e4b5f4713b875baca13df29.PNG' alt='anjam midam' />
+      <div className="fs-1">انجام میدم</div>
+      <div>این صفحه با درخواست سایت "انجام میدم" ایجاد شده است</div>
+    </div>
+  )
+}
